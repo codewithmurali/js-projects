ce28b2f0f291cdfc62bdae04b8d0685e29213010
codewithmurali <muralikumar.admire@gmail.com>
2023-01-11 01:29:04 +0530

    made some corrections

---
+++ Login form/script.js
@@ -0,0 +1,9 @@
+const username = document.getElementById("user__input");
+const password = document.getElementById("pwd");
+const usernameErr = document.getElementById("user__err");
+const passwordErr = document.getElementById("pass__err");
+
+function check() {
+  if (username.value == "codewithmurali" && password.value == 123) {
+  }
+}
