e8ab368089f8de18a7df8c3976fd4da012336152
codewithmurali <muralikumar.admire@gmail.com>
2023-01-20 23:46:07 +0530

    After commiting validation

--- Login form/script.js
+++ Login form/script.js
@@ -1,9 +1,26 @@
-const username = document.getElementById("user__input");
-const password = document.getElementById("pwd");
-const usernameErr = document.getElementById("user__err");
-const passwordErr = document.getElementById("pass__err");
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const usernameErrMsg = document.getElementById("user__err");
+const passwordErrMsg = document.getElementById("pass__err");
+const btn = document.getElementById("btn");
 
-function check() {
-  if (username.value == "codewithmurali" && password.value == 123) {
+form.addEventListener("submit", (e) => {
+  check(e);
+});
+function check(e) {
+  const usernameValue = username.value.trim();
+  const passwordValue = password.value.trim();
+  if (usernameValue != "codewithmurali") {
+    e.preventDefault();
+    usernameErrMsg.style.display = "block";
+  }
+  if (passwordValue != "1234") {
+    e.preventDefault();
+    passwordErrMsg.style.display = "block";
+  }
+  if (usernameValue == "codewithmurali" && passwordValue == "1234") {
+    form.action = "./home.html";
+    // console.log("success");
   }
 }
